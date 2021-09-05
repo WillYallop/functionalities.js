6703b4a050b3993f28a6b1b7023bad44d5145d7a
William Yallop <hello@williamyallop.com>
2021-09-05 11:37:34 +0100

    Added propeties to store slider elements, and updated verifyConfig to now verify elements as well

--- src/utilities/slider/index.ts
+++ src/utilities/slider/index.ts
@@ -2,6 +2,7 @@
 import error from '../../shared/error';
 
 // Specific
+import './style/main.scss';
 import { touchEventsInitiate, touchEventsDestroy } from './handler/touch-events';
 
 // Type definitions - only import facing ones
@@ -21,7 +22,7 @@ interface Config {
     enableTouch?: boolean,
     classes?: {
         slider?: string,
-        wrapper?: string
+        wrapper?: string,
         slide?: string
     },
     triggerCB?: (response: string) => void
@@ -31,6 +32,10 @@ interface Config {
 export default class Slider {
     config: Config;
     touchEventsInitiate;
+    // Elements
+    sliderElement: HTMLElement;
+    wrapperElement: HTMLElement;
+    slidesElementsArray: NodeListOf<HTMLElement>;
     constructor(config: Config) {
         this.config = {
             id: 'sliderID',
@@ -47,12 +52,14 @@ export default class Slider {
             },
             ...config
         }
+        // Store the elements we'll need to interact with
+        this.sliderElement = document.getElementById(this.config.id);
+        this.wrapperElement = this.sliderElement.querySelector(`.${this.config.classes.wrapper}`);
+        if(this.wrapperElement) this.slidesElementsArray = this.wrapperElement.querySelectorAll(`.${this.config.classes.slide}`);
         // Init
-        if(!this.verifyConfig()) this.initialise(); 
+        if(!this.verify()) this.initialise(); 
     } 
     initialise() {
-        // Store the elements we'll need to interact with
-
         // Adjust slides based on config.perPage so everything is translated and overflowing correctly
 
         // Set events to handle interacting with the slider - mobile and mouse touch events
@@ -86,30 +93,36 @@ export default class Slider {
         if(this.config.enableTouch) touchEventsDestroy();
     }
 
-    // Verify config values for non typescript implementation
-    verifyConfig():boolean {
+    // Verify values for non typescript implementation
+    verify():boolean {
         let hasError:boolean = false;
         // config.id
         if(typeof this.config.id != 'string') error(`Typeof "${typeof this.config.id }" is not allow for "id". It must be type "string".`), hasError = true;
         // config.perPage
-        if(typeof this.config.perPage != 'number') if(this.config.perPage != 'auto') error(`Typeof "${typeof this.config.perPage }" is not allow for "perPage". It must be type "number".`), hasError = true;
+        if(typeof this.config.perPage != 'number') if(this.config.perPage != 'auto') error(`Typeof "${typeof this.config.perPage }" is not allow for "perPage". It must be type "number"!`), hasError = true;
         // config.direction
-        if(typeof this.config.direction != 'string') error(`Typeof "${typeof this.config.direction }" is not allow for "direction". It must be type "string".`), hasError = true;
-        else if(this.config.direction != ConfigDirection.vertical && this.config.direction != ConfigDirection.horizontal) error(`"direction" can only be equal to ${ConfigDirection.vertical} or ${ConfigDirection.horizontal}.`), hasError = true;
+        if(typeof this.config.direction != 'string') error(`Typeof "${typeof this.config.direction }" is not allow for "direction". It must be type "string"!`), hasError = true;
+        else if(this.config.direction != ConfigDirection.vertical && this.config.direction != ConfigDirection.horizontal) error(`"direction" can only be equal to ${ConfigDirection.vertical} or ${ConfigDirection.horizontal}!`), hasError = true;
         // config.loop
-        if(typeof this.config.loop != 'boolean') error(`Typeof "${typeof this.config.loop }" is not allow for "loop". It must be type "boolean".`), hasError = true;
+        if(typeof this.config.loop != 'boolean') error(`Typeof "${typeof this.config.loop }" is not allow for "loop". It must be type "boolean"!`), hasError = true;
         // config.autoPlay
-        if(typeof this.config.autoPlay != 'boolean') error(`Typeof "${typeof this.config.autoPlay }" is not allow for "autoPlay". It must be type "boolean".`), hasError = true;
+        if(typeof this.config.autoPlay != 'boolean') error(`Typeof "${typeof this.config.autoPlay }" is not allow for "autoPlay". It must be type "boolean"!`), hasError = true;
         // config.speed
-        if(typeof this.config.speed != 'number') error(`Typeof "${typeof this.config.speed }" is not allow for "speed". It must be type "number".`), hasError = true;
+        if(typeof this.config.speed != 'number') error(`Typeof "${typeof this.config.speed }" is not allow for "speed". It must be type "number"!`), hasError = true;
         // config.enableTouch
-        if(typeof this.config.enableTouch != 'boolean') error(`Typeof "${typeof this.config.enableTouch }" is not allow for "enableTouch". It must be type "boolean".`), hasError = true;
+        if(typeof this.config.enableTouch != 'boolean') error(`Typeof "${typeof this.config.enableTouch }" is not allow for "enableTouch". It must be type "boolean"!`), hasError = true;
         // config.classes
-        if(typeof this.config.classes.slider != 'string') error(`Typeof "${typeof this.config.classes.slider }" is not allow for "classes.slider". It must be type "string".`), hasError = true;
-        if(typeof this.config.classes.wrapper != 'string') error(`Typeof "${typeof this.config.classes.wrapper }" is not allow for "classes.wrapper". It must be type "string".`), hasError = true;
-        if(typeof this.config.classes.slide != 'string') error(`Typeof "${typeof this.config.classes.slide }" is not allow for "classes.slide". It must be type "string".`), hasError = true;
+        if(typeof this.config.classes.slider != 'string') error(`Typeof "${typeof this.config.classes.slider }" is not allow for "classes.slider". It must be type "string"!`), hasError = true;
+        if(typeof this.config.classes.wrapper != 'string') error(`Typeof "${typeof this.config.classes.wrapper }" is not allow for "classes.wrapper". It must be type "string"!`), hasError = true;
+        if(typeof this.config.classes.slide != 'string') error(`Typeof "${typeof this.config.classes.slide }" is not allow for "classes.slide". It must be type "string"!`), hasError = true;
         // config.triggerCB
-        if(typeof this.config.triggerCB != 'function') error(`Typeof "${typeof this.config.triggerCB }" is not allow for "triggerCB". It must be type "function".`), hasError = true;
+        if(typeof this.config.triggerCB != 'function') error(`Typeof "${typeof this.config.triggerCB }" is not allow for "triggerCB". It must be type "function"!`), hasError = true;
+
+        // Verify Elements
+        if(!this.sliderElement) error(`Cannot find slider element of ID: "${this.config.id}"!`), hasError = true;
+        if(!this.wrapperElement) error(`Cannot find slider wrapper element of Class: "${this.config.classes.wrapper}"!`), hasError = true;
+        if(!this.slidesElementsArray) error(`Cannot find slides for slider with ID: "${this.config.id}"! This might be beacuse your "${this.config.classes.wrapper}" is undefined!`), hasError = true;
+        else if(!this.slidesElementsArray.length) error(`You currently have 0 slides for the slider with ID: "${this.config.id}"!`), hasError = true;
 
         return hasError;
     }
