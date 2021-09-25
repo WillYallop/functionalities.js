4985e78d8e2e0b0f7dea98672870935a04d8aa57
William Yallop <hello@williamyallop.com>
2021-09-25 17:53:14 +0100

    Added config to the form-validation utility, and added type verification to it and error handling

--- src/utilities/form-validation/index.ts
+++ src/utilities/form-validation/index.ts
@@ -1,5 +1,21 @@
+interface CustomValidatorObj {
+    methodName: string,
+    regex: RegExp | false,
+    minChar: number | false,
+    maxChar: number | false,
+    validator?: (value:any) => boolean; 
+}
+
 interface FormValidationConfig {
     id?: string;
+    onKeyup?: boolean;
+    escapeValues?: boolean;
+    inputClasses?: {
+        valid?: string,
+        error?: string
+    };
+    customValidators?: Array<CustomValidatorObj>;
+    onVerify?: () => void
 }
 enum VerificationMethods { 
     email = 'email', 
@@ -18,12 +34,13 @@ enum InputTypes {
 }
 type ValidatorClasses = EmailValidator | NameValidator | CustomValidator | PhoneValidator | AddressValidator | LongTextValidator | false;
 interface InputObj {
-    passed: boolean;
     method: VerificationMethods | false,
     element: HTMLElement,
     type: InputTypes,
     validator: ValidatorClasses
 }
+
+
 
 
 // General
@@ -59,13 +76,21 @@ export default class FormValidation {
     inputs: Map<string, InputObj>;
     constructor(id: string, config: FormValidationConfig) {
         this.config = {
+            onKeyup: true,
+            escapeValues: false,
+            inputClasses: {
+                valid: 'valid',
+                error: 'error'
+            },
             ...config
         };
         this.config.id = id;
+        if(this.config.inputClasses.valid === undefined) this.config.inputClasses.valid  = 'valid';
+        if(this.config.inputClasses.error === undefined) this.config.inputClasses.error  = 'error';
         this.inputs = new Map();
         
         this.formElement = document.getElementById(id);
-        if(this.verifyConfig) this.initialise(); // do work
+        if(this.verifyConfig()) this.initialise(); // do work
     }
     initialise() {
         // Add supported elements in the form to the inputs map
@@ -74,16 +99,12 @@ export default class FormValidation {
             // if valid input type
             let validElement = inputTypes.find( x => x === element.localName );
             if(validElement) {
-
                 // If the element has no ID skip it.
                 // If the validation method is not set, we do not validate than input.
                 if(element.id) {
                     var validationMethod: false | VerificationMethods;
                     var validator: ValidatorClasses;
                     let hasMethod = validationMethods.find( x => x === element.getAttribute('validation-method'));
-                    console.log(hasMethod);
-
-
                     if(hasMethod) {
                         switch(hasMethod) {
                             case VerificationMethods.email: {
@@ -117,12 +138,8 @@ export default class FormValidation {
                         validationMethod = false;
                         validator = false;
                     };
-
-           
-
                     // Set new input in map
                     this.inputs.set(element.id, {
-                        passed: false,
                         method: validationMethod,
                         element: element,
                         type: validElement,
@@ -140,6 +157,17 @@ export default class FormValidation {
         let hasError:boolean = false;
         // config.id
         if(typeof this.config.id != 'string') error(`Typeof "${typeof this.config.id }" is not allow for "id". It must be type "string".`), hasError = true;
+        // config.customValidators
+        if(this.config.customValidators != undefined) if(!Array.isArray(this.config.customValidators)) error(`"config.customValidators" must be an array. Refer to the documentation for more information.`), hasError = true;
+        // config.onKeyup
+        if(typeof this.config.onKeyup != 'boolean') error(`Typeof "${typeof this.config.onKeyup }" is not allow for "onKeyup". It must be type "boolean"!`), hasError = true;
+        // config.escapeValues
+        if(typeof this.config.escapeValues != 'boolean') error(`Typeof "${typeof this.config.escapeValues }" is not allow for "escapeValues". It must be type "boolean"!`), hasError = true;
+        // config.inputClasses
+        if(typeof this.config.inputClasses.valid != 'string') error(`Typeof "${typeof this.config.inputClasses.valid }" is not allow for "inputClasses.valid". It must be type "string"!`), hasError = true;
+        if(typeof this.config.inputClasses.error != 'string') error(`Typeof "${typeof this.config.inputClasses.error }" is not allow for "inputClasses.error". It must be type "string"!`), hasError = true;
+        // config.clickEvent
+        if(this.config.onVerify != undefined) if(typeof this.config.onVerify != 'function') error(`Typeof "${typeof this.config.onVerify }" is not allow for "onVerify". It must be type "function"!`), hasError = true;
 
         return hasError;
     }
@@ -147,6 +175,10 @@ export default class FormValidation {
 
     // External function
     verify() {
+
+        // Loop through each entry in the inputs map
+        // Run the validator
+        // Return response
 
     }
 }
